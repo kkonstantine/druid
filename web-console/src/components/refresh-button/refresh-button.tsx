99b7f38559ccd19f865c4b1e81d1720ba8e45728
Lokesh Lingarajan <llingarajan@confluent.io>
2022-03-17 14:01:08 -0700

    Setting default refresh value for task view as none. As part of this we added a default parameter that can be passed for refresh widget to avoid every refresh widget getting affected.

--- web-console/src/components/refresh-button/refresh-button.tsx
+++ web-console/src/components/refresh-button/refresh-button.tsx
@@ -34,14 +34,15 @@ const DELAYS: DelayLabel[] = [
 export interface RefreshButtonProps {
   onRefresh: (auto: boolean) => void;
   localStorageKey?: LocalStorageKeys;
+  defaultDelay?: number;
 }
 
 export const RefreshButton = React.memo(function RefreshButton(props: RefreshButtonProps) {
-  const { onRefresh, localStorageKey } = props;
+  const { onRefresh, localStorageKey, defaultDelay = 30000 } = props;
 
   return (
     <TimedButton
-      defaultDelay={30000}
+      defaultDelay={defaultDelay}
       label="Auto refresh every"
       delays={DELAYS}
       icon={IconNames.REFRESH}
